c3bfe6236eb5b58cd80bb59e8a427174db5ecaa9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-25 08:55:25 +0700

    Updated the sandbox

---
+++ python314/startertwo.py
@@ -0,0 +1,48 @@
+"""Module providing a sandbox"""
+
+# print("hello world")
+# A = 2 + 3
+# print(A)
+#  import random
+
+# random_integer = random.randint(1, 10)
+# random_float = random.random()
+# random_float_undr_5 = random.uniform(0, 5)
+# random_under = random.random()
+
+# # print(random_integer)
+# # print(random_float)
+# # print(random_float_undr_5)
+# result = round((random_under * 5), 2)
+# print(result)
+
+
+# -----
+
+# rand1 = random.randint(1, 2)
+# rand = round(random.random())
+# if rand == 1:
+#     COIN = "Head"
+# else:
+#     COIN = "Tail"
+# print(f"You got a {COIN}")
+# print(rand)
+# ----
+
+# Lists
+# import random
+
+# # group = ["a", "b", "c", "d"]
+# STR_INP = "adam, bob, clio, don"
+# # using choice
+# group = STR_INP.split(", ")
+# print("---------")
+# print(group)
+# selected = random.choice(group)
+# print(selected)
+
+# # without choice
+# people_num = len(group)
+# selected_index = random.randint(0, people_num - 1)
+# selected2 = group[selected_index]
+# print(selected2)
