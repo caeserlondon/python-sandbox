57fd15be608505741a25396aaa003581ea14c1ea
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-26 13:45:01 +0700

    Updated the starter

---
+++ python314/starter.py
@@ -0,0 +1,34 @@
+parrot ="Norwegian Blue"
+
+# Indexing
+#print(parrot[3].capitalize())
+
+# Slice
+# print(parrot[0:6])
+#print(parrot[0:9])
+#print(parrot[:9])
+#print(parrot[10:])
+#print(parrot[:6] + parrot[6:])
+#print(parrot[:])
+#print(parrot[-4:-2])
+print(parrot[-4:12])
+
+print(parrot[4:12:2])
+
+# Slice back
+print(parrot[::-1])
+print(parrot[-1:-9:-1])
+print(parrot[:-9:-1])
+print(parrot[-8::].capitalize())
+print(parrot[-1:])
+
+
+today = "friday"
+#print("day" in today)
+#print("f" in parrot)
+print("r" in parrot)
+print("o" in parrot)
+print("n" in parrot)
+
+
+
