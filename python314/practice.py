9aaeba2a03cc10adcd244191356a04cf3d3cc072
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-26 13:44:45 +0700

    Updated the practice

---
+++ python314/practice.py
@@ -0,0 +1,9 @@
+# x = 1
+
+# print("To infinity and beyond! We're getting close, on %d now!" % (x))
+# x += 1
+
+
+# for x in range(1, 4):
+#     for y in range(1, 5):
+#         print('%d * %d = %d' % (y, x, x*y))
