fa483f8da7de670328b20fe4fbf12e8e9e12992c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-04 11:23:24 +0700

    Uddated the alphabet

--- python314/alphabet-phonetic/main.py
+++ python314/alphabet-phonetic/main.py
@@ -4,12 +4,31 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+# have_question = True
+# while have_question:
+#     word = input("Please, Enter a word: ").upper()
+#     try:
+#         output_list = [phonetic_dict[letter] for letter in word]
+#     except KeyError:
+#         print("Invalid word")
+#     else:
+#         print(output_list)
+#         have_question = False
+
+def generate_phonetic():
+    word = input("Please, Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Invalid word")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
