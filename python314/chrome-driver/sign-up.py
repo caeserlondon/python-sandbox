a91c4ad362b086b811cba07ceb2c244d03d9f604
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-09 15:47:57 +0700

    Updated sign up

--- python314/chrome-driver/sign-up.py
+++ python314/chrome-driver/sign-up.py
@@ -7,4 +7,38 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
-driver.get("https://www.wikipedia.org/")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+#
+# first_name = wait.until(
+#     EC.visibility_of_element_located((By.NAME, "fname"))
+# )
+# first_name.send_keys("Caeser")
+# first_name.send_keys(Keys.ENTER)
+#
+# last_name = wait.until(
+#     EC.visibility_of_element_located((By.NAME, "lname"))
+# )
+# last_name.send_keys("Ibrahim")
+# last_name.send_keys(Keys.ENTER)
+#
+# email = wait.until(
+#     EC.visibility_of_element_located((By.NAME, "email"))
+# )
+# email.send_keys("caeser@gmail.com")
+# email.send_keys(Keys.ENTER)
+
+first_name = driver.find_element(By.CLASS_NAME, "top")
+last_name = driver.find_element(By.CLASS_NAME, "middle")
+email = driver.find_element(By.CLASS_NAME, "bottom")
+button = driver.find_element(By.CLASS_NAME, "btn")
+
+first_name.send_keys("Caeser")
+first_name.send_keys(Keys.ENTER)
+last_name.send_keys("Ibrahim")
+last_name.send_keys(Keys.ENTER)
+email.send_keys("caeser@gmail.com")
+email.send_keys(Keys.ENTER)
+
+
+
+button.click()
