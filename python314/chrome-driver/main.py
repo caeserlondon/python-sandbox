64753658da8cb85e15c14e92987f2eebc3725288
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-09 13:28:59 +0700

    Updated the main chrome driver

--- python314/chrome-driver/main.py
+++ python314/chrome-driver/main.py
@@ -1,11 +1,40 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+#
+# chrome_driver_path = "/Users/caeseribrahim/Documents/projects/chromedriver-mac-arm64/chromedriver"
+#
+# service = Service(executable_path=chrome_driver_path)
+# driver = webdriver.Chrome(service=service)
+
+
+
+
+#Import the necessary modules
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-chrome_driver_path = "/Users/caeseribrahim/Documents/projects/chromedriver-mac-arm64/chromedriver"
+#Initialize the WebDriver
+driver = webdriver.Chrome()
 
-service = Service(executable_path=chrome_driver_path)
-driver = webdriver.Chrome(service=service)
-driver.get("https://amazon.com")
+#Open page
+driver.get("https://www.python.org/")
+
+#Locate the element
+# element = driver.find_element(By.CLASS_NAME,"icon_calendar")
+# title = driver.find_elements(By.CLASS_NAME, "icon-calendar")
+# print(title)
+
+# logo = driver.find_element(By.CLASS_NAME, "python-logo")
+# print(logo.size)
+
+# docs_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
+# print(docs_link.text)
+#
+# bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+#
+# print(bug_link.get_attribute("href"))
+#
+# print(bug_link.text)
 
 # driver.close()
 driver.quit()
