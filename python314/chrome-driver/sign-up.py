270f84d6e973eab2b2ce18df48bf7168c03dafcd
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-10 10:19:50 +0700

    Updated the sign up

--- python314/chrome-driver/sign-up.py
+++ python314/chrome-driver/sign-up.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
+# wait = WebDriverWait(driver, 10)
 driver.get("https://secure-retreat-92358.herokuapp.com/")
 #
 # first_name = wait.until(
@@ -33,11 +33,9 @@ email = driver.find_element(By.CLASS_NAME, "bottom")
 button = driver.find_element(By.CLASS_NAME, "btn")
 
 first_name.send_keys("Caeser")
-first_name.send_keys(Keys.ENTER)
 last_name.send_keys("Ibrahim")
-last_name.send_keys(Keys.ENTER)
 email.send_keys("caeser@gmail.com")
-email.send_keys(Keys.ENTER)
+
 
 
 
