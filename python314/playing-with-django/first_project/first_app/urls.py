7aa62fb49bc18bd1fe6fbf1da4c913eaba1d5629
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-15 09:43:43 +0700

    Add the users new path

--- python314/playing-with-django/first_project/first_app/urls.py
+++ python314/playing-with-django/first_project/first_app/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    re_path(r'^$', views.help , name='help'),
+    # path('', views.index, name='index'),
+    path('', views.users, name='users'),
+
 ]
 
