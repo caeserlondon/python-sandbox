ca4421a9b5fd95b799ea8fd71e750398d0587345
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-15 09:44:35 +0700

    add the comanies and the users views

--- python314/playing-with-django/first_project/first_app/views.py
+++ python314/playing-with-django/first_project/first_app/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord, User
 
 
 # Create your views here.
+def companies(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {"access_records": webpages_list}
+
+    return render(request, "first_app/companies.html", context=date_dict)
+
 def index(request):
-    my_dect = {
-        "insert_me":'Hello from the app veiews.py',
-        "my_name":'caeser'
-    }
-    return render(request, "first_app/index.html", context=my_dect)
+    return render(request, "first_app/index.html")
 
-def hello(request):
-    return HttpResponse("<h1>Hello, Hello, Hello</h1>")
-
-def help(request):
+def help_me(request):
     helpDict = {'help_insert':"HELP PAGE"}
     return render(request, "help/help.html", context=helpDict)
+
+def users(request):
+    users_list = User.objects.order_by('first_name')
+    users_dict ={ "users": users_list}
+    return render(request, "first_app/users.html", context=users_dict)
 
 
 
