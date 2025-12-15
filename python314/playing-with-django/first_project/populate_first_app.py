3c3cfec3968315abd20367276aa50753afb860e7
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-12-15 09:42:12 +0700

    Add the users uploader

--- python314/playing-with-django/first_project/populate_first_app.py
+++ python314/playing-with-django/first_project/populate_first_app.py
@@ -6,7 +6,7 @@ django.setup()
 
 ## Fake Pop Script
 import random
-from first_app.models import AccessRecord, Webpage, Topic
+from first_app.models import AccessRecord, Webpage, Topic, User
 from faker import Faker
 
 fake_gen = Faker()
@@ -20,20 +20,28 @@ def add_topic():
 
 def populate(N=5):
     for _ in range(N):
-        #Add a topic
-        top = add_topic()
+        ####  FOR TOPICS
+        # #Add a topic
+        # top = add_topic()
+        #
+        # #Create the fake data
+        # fake_url = fake_gen.url()
+        # fake_date = fake_gen.date()
+        # fake_name = fake_gen.company()
+        #
+        # # Creat the new page entry
+        # webpage = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+        #
+        # #Creat a fake access record for that webpage
+        # acc_rec = AccessRecord.objects.get_or_create(name=webpage, date=fake_date)[0]
 
-        #Create the fake data
-        fake_url = fake_gen.url()
-        fake_date = fake_gen.date()
-        fake_name = fake_gen.company()
-
-        # Creat the new page entry
-        webpage = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
-
-        #Creat a fake access record for that webpage
-        acc_rec = AccessRecord.objects.get_or_create(name=webpage, date=fake_date)[0]
-
+        #### FOR USERS
+        fake_name = fake_gen.name().split()
+        fake_first_name = fake_name[0]
+        fake_last_name = fake_name[1]
+        fake_email = fake_gen.email()
+        # New User
+        user = User.objects.get_or_create(first_name=fake_first_name, last_name=fake_last_name, email=fake_email)[0]
 
 if __name__ == '__main__':
     print("Populating...")
